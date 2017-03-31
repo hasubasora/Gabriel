8d5bb02fab9be9815ecd15a07f0e8676fd6c040a
chenyongh <chenyongh@sunaw.net>
2017-03-31 18:18:57 +0800

    2017-3-31 18:18:53

--- Vue/dist/javascripts/main.js
+++ Vue/dist/javascripts/main.js
@@ -606,6 +606,17 @@ window.onload = function () {
 };
 "use strict";
 
+(function () {
+    var ft = document.getElementsByTagName("html")[0]; //获取到html标签
+    var s = window.screen.width; //获取屏幕的宽度
+    window.onresize = function () {
+        //屏幕尺寸改变触发
+        var w = document.body.offsetWidth; //获取浏览器内容的宽度
+        ft.style.fontSize = w / s * 16 + "px";
+    };
+})();
+"use strict";
+
 /**
  * last month chenyongh 2016年12月24日14:31:55
  */
