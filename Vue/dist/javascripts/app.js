251434787fbf6ab0449dddc45bd8cca65b776de7
chenyongh <chenyongh@sunaw.net>
2017-04-08 22:42:34 +0800

    2561

--- Vue/dist/javascripts/app.js
+++ Vue/dist/javascripts/app.js
@@ -1,36 +1,46 @@
- var len = document.getElementsByTagName('td').length;
- var tag = document.getElementsByTagName('td')
- for (var t = 0; t < len; t++) {
-     tag[t].addEventListener('click', function(e) {
-         this.setAttribute('contenteditable', 'true');
-         this.focus();
-         e.cancelBubble = true;
-     });
-
- }
- //取消后的
- document.onclick = function(event) {
-     event = event || window.event;
-     var target = event.target || event.srcElement;
-     console.log(target.nodeName)
-     if (target.nodeName == target.nodeName) {
-         for (var o = 0; o < len; o++) {
-             tag[o].removeAttribute('contenteditable');
+ let sf = {
+     config: {
+         tbodys: document.getElementsByTagName('tbody')[0],
+         tag: document.getElementsByTagName('td'),
+         len: document.getElementsByTagName('td').length,
+         btn: document.getElementById('btn'),
+         thead: document.getElementsByTagName('thead')[0]
+     },
+     init() {
+         document.addEventListener('click', sf.unbodys)
+         sf.config.tbodys.addEventListener('click', sf.bodys)
+         sf.config.btn.addEventListener('click', sf.addBodys)
+     },
+     bodys(event) {
+         event = event || window.event;
+         var target = event.target || event.srcElement;
+         console.log(sf.config.tag[0].nodeName)
+         if (target.nodeName == sf.config.tag[0].nodeName) {
+             target.setAttribute('contenteditable', 'true');
+             target.focus();
+             event.cancelBubble = true;
+         }
+     },
+     unbodys(event) {
+         event = event || window.event;
+         var target = event.target || event.srcElement;
+         // console.log(target.nodeName)
+         if (target.nodeName == target.nodeName) {
+             for (let o = 0, i = sf.config.len; o < i; o++) {
+                 sf.config.tag[o].removeAttribute('contenteditable');
+             }
+         }
+     },
+     addBodys() {
+         var thLen = sf.config.thead.getElementsByTagName('th').length;
+         // insertRow
+         var tbody = document.getElementsByTagName('tbody')[0].insertRow(0);
+         tbody.setAttribute('role', 'row');
+         for (var r = 0; r < thLen; r++) {
+             var y = tbody.insertCell(r);
+             y.innerHTML = "&nbsp;";
          }
      }
- };
 
- //创建
- var btn = document.getElementById('btn');
- var thead = document.getElementsByTagName('thead')[0];
- var th = thead.getElementsByTagName('th')
- var thLen = th.length;
- btn.onclick = function() {
-     // insertRow
-     var tbody = document.getElementsByTagName('tbody')[0].insertRow(0);
-     tbody.setAttribute('role', 'row');
-     for (var r = 0; r < thLen; r++) {
-         var y = tbody.insertCell(r);
-         y.innerHTML = "&nbsp;";
-     }
+
  }
