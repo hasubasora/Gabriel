e17365ba4f651f5088c03ff7a4ef88bd14be77e7
chenyongh <chenyongh@sunaw.net>
2017-04-11 17:49:11 +0800

    56789

--- Vue/src/javascripts/filter.js
+++ Vue/src/javascripts/filter.js
@@ -3,7 +3,7 @@ let filter = {
      * @param  { String } str 需要转义的字符串
      * @returns { String }     转义后的字符串 
      */
-    unhtmlForUrl(str) {
+    unhtml(str) {
         return str ? str.replace(/[<">']/g, (a) => {
             return {
                 '<': '&lt;',
@@ -19,8 +19,16 @@ let filter = {
      * @param {String} reg 正则
      * @returns {bool}
      */
-    VerificationPhone(tel, reg) {
+    verificationPhone(tel, reg) {
         return tel ? reg || /^0?1[3|4|5|7|8][0-9]\d{8}$/.test(tel) : '';
         // console.log(reg.test(tel))
+    },
+    /**
+     * 去除多余空格
+     * @param { String } str 需要去空格的字符串 
+     */
+    unBlank(str) {
+        return str ? str.replace(/\s/ig, '') : '';
     }
+
 }
